04935a56870d91458f89cd37340d1a47f3110586
Ion Pasea <ionpasea@gmail.com>
2022-10-13 16:45:25 -0400

    adding sonar to circleci config.yml 27

--- HelloWorld/sonar_run.sh
+++ HelloWorld/sonar_run.sh
@@ -4,7 +4,7 @@ docker-compose -f docker-compose-sonar.yml build
 docker-compose -f docker-compose-sonar.yml up -d
 # sudo apt-get install jq -y
 PROJECTKEY="sonarbackend"
-Check=`curl -s -u admin:admin123 http://142.126.144.195:9000/api/qualitygates /project_status?projectKey=$PROJECTKEY | jq '.projectStatus.status' | tr -d '"'`
+Check=`curl -s -u admin:admin http://localhost:9000/api/qualitygates /project_status?projectKey=$PROJECTKEY | jq '.projectStatus.status' | tr -d '"'`
 max_retry=30
 counter=0
 until [ "$Check" == "OK" ] || [ "$Check" = "ERROR" ] || [ "$Check" ==  "WARN" ];
@@ -14,9 +14,9 @@ until [ "$Check" == "OK" ] || [ "$Check" = "ERROR" ] || [ "$Check" ==  "WARN" ];
  echo "Retrying. Try #$counter"
  ((counter++))
 
-Check=`curl -s -u admin:admin123 http://142.126.144.195:9000/api/qualitygates /project_status?projectKey=$PROJECTKEY | jq '.projectStatus.status' | tr -d '"'`
+Check=`curl -s -u admin:admin http://localhost:9000/api/qualitygates /project_status?projectKey=$PROJECTKEY | jq '.projectStatus.status' | tr -d '"'`
 done
-QGSTATUS=`curl -s -u admin:admin123 http://142.126.144.195:9000/api/qualitygates /project_status?projectKey=$PROJECTKEY | jq '.projectStatus.status' | tr -d '"'`
+QGSTATUS=`curl -s -u admin:admin http://localhost:9000/api/qualitygates /project_status?projectKey=$PROJECTKEY | jq '.projectStatus.status' | tr -d '"'`
 if [ "$QGSTATUS" = "OK" ]
 then
 echo "Status is OK"
@@ -25,10 +25,10 @@ docker-compose -f docker-compose-sonar.yml down --rmi all
 echo "Sonar scanning has successfully ended with the status 'OK'"
 elif [ "$QGSTATUS" = "WARN" ]
 then
-echo "Status is WARN. Check out the quality of the products at  http://142.126.144.195:9000"
+echo "Status is WARN. Check out the quality of the products at  http://localhost:9000"
 exit 1 # terminate and indicate error
 elif [ "$QGSTATUS" = "ERROR" ]
 then
-echo "Status is ERROR. Check out the quality of the products at  http://142.126.144.195:9000"
+echo "Status is ERROR. Check out the quality of the products at  http://localhost:9000"
 exit 1 # terminate and indicate error
 fi
